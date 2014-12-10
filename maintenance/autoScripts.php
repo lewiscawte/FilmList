7036f25745dbe4b4afb7b0b19aacc553c522d421
Lewis Cawte <lewis@lewiscawte.me>
2014-12-10 15:11:14 +0000

    Use DB object and document flagAdmin()

--- maintenance/autoScripts.php
+++ maintenance/autoScripts.php
@@ -16,9 +16,22 @@ class autoScripts extends Context {
 		$this->config = $this->getConfig();
 	}
 
+	/**
+	 * @param $jobDetails
+	 * @param $status
+	 * Status codes:
+	 * 1 - Verified data sources do not contain the information the
+	 *     script is searching for.
+	 * 2 - Other enabled data sources do not contain the requested
+	 *     data.
+	 * 3 - Data found but already data in the main field. Data
+	 *     added to the proposal table for admin review.
+	 * 4 - Data added directly to the field as no data found.
+	 * @return mixed
+	 */
 	protected function flagAdmin( $jobDetails, $status ) {
 
-		return $db->insert(
+		return $this->db->insert(
 			'admin_notifications',
 			$jobDetails['jobId'],
 			$jobDetails['jobFilm'],
