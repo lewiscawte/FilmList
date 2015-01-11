84c5e9d1ba8ab0dfe050616fa19e61c64ab468d0
Lewis Cawte <lewis@lewiscawte.me>
2015-01-11 10:56:07 +0000

    Add new redirect code

--- inc/Linker.php
+++ inc/Linker.php
@@ -14,6 +14,11 @@ class Linker extends Context {
 		$this->conf = $this->getConfig();
 	}
 
+	/**
+	 * @param $page
+	 * @param $ref
+	 * @return bool|mysqli_result|string
+	 */
 	public static function intURLConstruct( $page, $ref ) {
 		$conf = new Config();
 		$returnURL = $conf->getSetting( 'BaseURL' );
@@ -28,5 +33,44 @@ class Linker extends Context {
 		}
 
 		return $returnURL;
+	}
+
+	/**
+	 * @param array $location - $page and $pagetype
+	 * @param array $extras - extra details, eg, src.
+	 */
+	public static function doRedirect( $location = array(), $extras = array() ) {
+		$conf = new Config();
+		$baseURL = $conf->getSetting( 'BaseURL' );
+
+		$baseURL .= '?page=' . $location['page'];
+
+		if( isset( $location['pagetype'] ) ) {
+			$baseURL =+ '&type=' . $location['pagetype'];
+		}
+
+		$redirectURL = $baseURL;
+
+		// Dump the location array as it could be huge, we're only
+		// after one key and value set.
+		// Dump string from memory as it's unused now.
+		unset( $location, $baseURL );
+
+		if ( isset( $extras ) ) {
+			// Get rid of junk
+			array_unique( $extras );
+
+			foreach ( $extras as $extra ) {
+				$key = htmlspecialchars( array_keys( $extra ) );
+				$value = htmlspecialchars( array_values( $extra ) );
+				unset( $extra );
+
+				$redirectURL .= '&' . $key . '=' . $value;
+				unset( $key, $value );
+			}
+		}
+
+		header( 'Location: ' . $redirectURL, true, 302 );
+		unset( $redirectURL );
 	}
 } 
