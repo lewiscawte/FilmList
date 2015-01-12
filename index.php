c4f1f0dc8bb0af056ec663372f452aefb0673881
Lewis Cawte <lewis@lewiscawte.me>
2015-01-12 11:14:20 +0000

    Add more pages

--- index.php
+++ index.php
@@ -12,15 +12,31 @@ require "settings.php";
 require "inc/FileLoader.php";
 
 if ( !isset( $_REQUEST['page'] ) || $_REQUEST['page'] === "index" ) {
-	$page = "index";
+	$page = 'index';
 	$newPage = new IndexPage();
 	$newPage->execute( $page );
-}
-
-/*
-if( !isset( $_REQUEST['film'] ) ) {
-	$fl->run();
 } else {
-	$fl->details( $_REQUEST['film'] );
+	$page = $_REQUEST['page'];
+	switch ( $page ) {
+		case 'error':
+			$newPage = new ErrorPage();
+			$newPage->execute( $page );
+			break;
+		case 'listfilms':
+			$newPage = new ListFilms();
+			$newPage->execute( $page );
+			break;
+		default:
+			Linker::doRedirect(
+				array(
+					'error',
+					'exceptionPoorPage'
+				),
+				array (
+					'page' => $page,
+					'src' => 'IndexFile',
+				)
+			);
+			break;
+	}
 }
-*/
