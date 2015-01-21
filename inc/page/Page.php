9d7a9cb205c24550ee978120e7b948b888605be1
Lewis Cawte <lewis@lewiscawte.me>
2015-01-21 10:00:13 +0000

    Title handling

--- inc/page/Page.php
+++ inc/page/Page.php
@@ -7,5 +7,29 @@
  */
 
 class Page extends Context {
+	// Put stuff here?
 
+	public function getTitle( $page ) {
+		switch ( $page ) {
+			case 'listfilms':
+				$title = 'List Films';
+				break;
+			case 'index':
+				$title = 'Home';
+				break;
+			default:
+				$title = $this->getConfig()->getSetting( 'Sitename' );
+				$title = $title['config_value'];
+				break;
+		}
+
+		$titleSitename = $this->getConfig()->getSetting( 'TitleSitename' );
+
+		if( boolval( $titleSitename['config_value'] ) ) {
+			$sitename = $this->getConfig()->getSetting( 'Sitename' );
+			$title .= " | " . $sitename['config_value'];
+		}
+
+		return $title;
+	}
 } 
