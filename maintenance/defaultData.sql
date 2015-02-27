ddcc18d95f336fc2d45f8758665e2584062c3227
Lewis Cawte <lewis@lewiscawte.me>
2015-02-27 10:07:43 +0000

    Missing file. Default/standard config items

---
+++ maintenance/defaultData.sql
@@ -0,0 +1,5 @@
+INSERT INTO `config` (`config_item`, `config_group`, `config_value`) VALUES
+  ('BaseImagePath', '', 'themes/FilmList'),
+  ('LoggedOutDash', '', 'false'),
+  ('Sitename', '', 'Family Film List'),
+  ('TitleSitename', '', '1');
